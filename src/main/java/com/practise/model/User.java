14f9168147178dfafe8d3d089928ad85815342d4
shivam <shivamgokarankar86@gmail.com>
2022-04-03 18:51:38 +0530

    Logic added to fetch data of users from database in JSON format

--- src/main/java/com/practise/model/User.java
+++ src/main/java/com/practise/model/User.java
@@ -3,46 +3,15 @@ package com.practise.model;
 public class User
 {
     private long userid;
-    private String firstName;
-    private String lastName;
-    private String emailid;
+    private String username;
+    private String password;
 
-    public User(long userid, String firstName, String lastName, String emailid) {
+
+    public User(long userid, String username, String password) {
         this.userid = userid;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailid = emailid;
+        this.username = username;
+        this.password = password;
     }
 
-    public long getUserid() {
-        return userid;
-    }
 
-    public void setUserid(long userid) {
-        this.userid = userid;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmailid() {
-        return emailid;
-    }
-
-    public void setEmailid(String emailid) {
-        this.emailid = emailid;
-    }
 }
