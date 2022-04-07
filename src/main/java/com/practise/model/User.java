1ac80eaea97494e795c53feff6b90809a4452b3d
shivam <shivamgokarankar86@gmail.com>
2022-04-07 15:00:06 +0530

    Angular js api call added JSON response from db table added

--- src/main/java/com/practise/model/User.java
+++ src/main/java/com/practise/model/User.java
@@ -3,15 +3,14 @@ package com.practise.model;
 public class User
 {
     private long userid;
-    private String firstName;
-    private String lastName;
-    private String emailid;
+    private String username;
+    private String password;
 
-    public User(long userid, String firstName, String lastName, String emailid) {
+
+    public User(long userid, String username, String password) {
         this.userid = userid;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailid = emailid;
+        this.username = username;
+        this.password = password;
     }
 
     public long getUserid() {
@@ -22,27 +21,19 @@ public class User
         this.userid = userid;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getPassword() {
+        return password;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmailid() {
-        return emailid;
-    }
-
-    public void setEmailid(String emailid) {
-        this.emailid = emailid;
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- src/main/java/com/practise/service/UserService.java
+++ src/main/java/com/practise/service/UserService.java
@@ -2,23 +2,24 @@ package com.practise.service;
 
 import com.practise.model.User;
 
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class UserService
 {
 
+    public ArrayList<User> getUsers(ResultSet rs, ArrayList<User> ul) throws SQLException {
 
+        while(rs.next())
+        {
+            User u1 = new User(rs.getLong(1),rs.getString(2),rs.getString(3));
+            ul.add(u1);
 
-
-
-
-
-    public List<User> getUsers()
-    {
-        return
-                Arrays.asList(new User(001l,"Shivam","Gokarankar","shivamg@unfyd.com")
-                ,new User(002l,"Snehal","Desai","snehald@unfyd.com")
-                ,new User(003l,"Rahul","Warke","rahulw@unfyd.com"));
+        }
+        return   ul;
     }
 }
